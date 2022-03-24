8c364f5655291c59abb12cb4fc0cd7ce16c6d731
padma <padma@DESKTOP-FM8OD3M>
2022-03-24 14:36:10 +0530

    second trail

---
+++ com.crm.Vtiger/src/test/java/com/crm/Testcases/CreateNameTest.java
@@ -0,0 +1,14 @@
+package com.crm.Testcases;
+
+import org.testng.annotations.Test;
+
+public class CreateNameTest {
+	
+	@Test
+	public void Createfirstname() {
+		
+		System.out.println("first name is hem");
+		
+	}
+
+}
